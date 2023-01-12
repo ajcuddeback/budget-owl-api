35f268224b2aec507fe7c61229811d274be1d436
austin-cuddeback <acuddeback@sohosquared.com>
2023-01-11 20:50:59 -0500

    Re-adding missing files

---
+++ src/main/java/com/budgetowl/demo/BudgetOwlApplication.java
@@ -0,0 +1,13 @@
+package com.budgetowl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetOwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetOwlApplication.class, args);
+	}
+
+}
